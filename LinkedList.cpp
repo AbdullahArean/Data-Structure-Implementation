f1db871bfff00cf56701a986ae02df7e72ffd434
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-02-22 23:11:56 +0600

    Some Indentation

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,27 +4,27 @@ class Node
 {
 public:
 	int data;
-    Node *next;
-    //Node *previous; 
+	Node *next;
+	// Node *previous;
 	Node()
 	{
 		data = 0;
 		next = NULL;
-        //previous = NULL;
+		// previous = NULL;
 	}
 	Node(int data)
 	{
 		this->data = data;
 		this->next = NULL;
-        //this->previous = NULL;
+		// this->previous = NULL;
 	}
 };
 class LinkedList
 {
 private:
-	Node 	*LinkedListHead;
-	int 	LengthOfLinkedList;
-	Node 	*ReverseRecursion(Node *current)
+	Node *LinkedListHead;
+	int LengthOfLinkedList;
+	Node *ReverseRecursion(Node *current)
 	{
 
 		if (current == NULL || current->next == NULL)
@@ -34,7 +34,7 @@ private:
 		current->next = NULL;
 		return newhead;
 	}
-	Node 	*ReverseIterative(Node *current)
+	Node *ReverseIterative(Node *current)
 	{
 		Node *prev = NULL, *next = NULL;
 		while (current != NULL)
@@ -46,130 +46,142 @@ private:
 		}
 		return prev;
 	}
-	void 	partition(Node *head, Node **front, Node **back)
-{
-	Node *fast, *slow;
-	if (head == NULL || head->next == NULL){
-		*front = head; // &a
-		*back = NULL; //&b
-	}
-	else{
-		slow = head;
-		fast = head->next;
-
-		while(fast!=NULL){
-			fast = fast->next;
-			if(fast != NULL) {
-				slow = slow->next;
-				fast = fast->next;
-			}
-
-		}
-		*front = head; // &a
-		*back = slow->next; //&b
-		slow->next = NULL;
-	}
-}
-	Node 	*mergeLists(Node* a, Node *b)
-{
-	Node * mergedList= NULL;
-	if (a == NULL){
-        return b;
-    }else if (b == NULL){
-        return a;
-    }
-
-	if(a->data<= b->data){
-		mergedList = a;
-		mergedList->next= mergeLists(a->next,b);
-	}
-	else 
+	void partition(Node *head, Node **front, Node **back)
 	{
-		mergedList =b;
-		mergedList->next = mergeLists(a, b->next);
+		Node *fast, *slow;
+		if (head == NULL || head->next == NULL)
+		{
+			*front = head; // &a
+			*back = NULL;  //&b
+		}
+		else
+		{
+			slow = head;
+			fast = head->next;
+
+			while (fast != NULL)
+			{
+				fast = fast->next;
+				if (fast != NULL)
+				{
+					slow = slow->next;
+					fast = fast->next;
+				}
+			}
+			*front = head;		// &a
+			*back = slow->next; //&b
+			slow->next = NULL;
+		}
 	}
-	return mergedList;
-}
-	void 	mergeSort(Node* *source){
-	Node* head= *source;
-	Node *a= NULL, *b= NULL;
-	if(head == NULL || head->next == NULL) return;
+	Node *mergeLists(Node *a, Node *b)
+	{
+		Node *mergedList = NULL;
+		if (a == NULL)
+		{
+			return b;
+		}
+		else if (b == NULL)
+		{
+			return a;
+		}
 
-	partition(head, &a, &b);
-	mergeSort(&a);
-	mergeSort(&b);
-
-	*source = mergeLists(a,b);
-
-}
-	bool 	detectLoop(Node * head){
-	Node * slow_p= head, *fast_p = head;
-	while(slow_p && fast_p && fast_p->next){
-		slow_p=slow_p->next;
-		fast_p= fast_p->next->next;
-		if(slow_p==fast_p) return 1;
+		if (a->data <= b->data)
+		{
+			mergedList = a;
+			mergedList->next = mergeLists(a->next, b);
+		}
+		else
+		{
+			mergedList = b;
+			mergedList->next = mergeLists(a, b->next);
+		}
+		return mergedList;
 	}
-	return 0;
-}
-	bool 	isPalindrome(Node *head)
-    {
-        Node* temp = head;
-        string str1, str2;
-        while(temp)
-        {
-            str1 = str1 + to_string(temp->data);
-            str2 = to_string(temp->data) + str2;
-            temp = temp->next;
-        }
-        
-        if(str1 == str2)
-        return true;
-        
-        return false;
-    }
-	Node* 	ReturnMiddle(Node *head){
-		//Delete middle is also easy
-        Node *slow_ptr = head;
-        Node *fast_ptr = head;
-		//Node * store = NULL; //To delete the middle element 
-  
-        if (head!=NULL)
-        {
-            while (fast_ptr && fast_ptr->next)
-            {
-                fast_ptr = fast_ptr->next->next;
-                slow_ptr = slow_ptr->next;
-				//if(fast_ptr->next) store = slow_ptr; //To delete the middle element 
-            }
-            //cout << "The middle element is [" << slow_ptr->data << "]" << endl; //To Print the middle element
-			//store->next= slow_ptr->next;//To delete the middle element 
-			
-        }
+	void mergeSort(Node **source)
+	{
+		Node *head = *source;
+		Node *a = NULL, *b = NULL;
+		if (head == NULL || head->next == NULL)
+			return;
+
+		partition(head, &a, &b);
+		mergeSort(&a);
+		mergeSort(&b);
+
+		*source = mergeLists(a, b);
+	}
+	bool detectLoop(Node *head)
+	{
+		Node *slow_p = head, *fast_p = head;
+		while (slow_p && fast_p && fast_p->next)
+		{
+			slow_p = slow_p->next;
+			fast_p = fast_p->next->next;
+			if (slow_p == fast_p)
+				return 1;
+		}
+		return 0;
+	}
+	bool isPalindrome(Node *head)
+	{
+		Node *temp = head;
+		string str1, str2;
+		while (temp)
+		{
+			str1 = str1 + to_string(temp->data);
+			str2 = to_string(temp->data) + str2;
+			temp = temp->next;
+		}
+
+		if (str1 == str2)
+			return true;
+
+		return false;
+	}
+	Node *ReturnMiddle(Node *head)
+	{
+		// Delete middle is also easy
+		Node *slow_ptr = head;
+		Node *fast_ptr = head;
+		// Node * store = NULL; //To delete the middle element
+
+		if (head != NULL)
+		{
+			while (fast_ptr && fast_ptr->next)
+			{
+				fast_ptr = fast_ptr->next->next;
+				slow_ptr = slow_ptr->next;
+				// if(fast_ptr->next) store = slow_ptr; //To delete the middle element
+			}
+			// cout << "The middle element is [" << slow_ptr->data << "]" << endl; //To Print the middle element
+			// store->next= slow_ptr->next;//To delete the middle element
+		}
 		return slow_ptr;
-    }
+	}
+
 public:
-				LinkedList()
+	LinkedList()
 	{
 		LinkedListHead = NULL;
 		LengthOfLinkedList = 0;
 	}
-				LinkedList(Node *head)
+	LinkedList(Node *head)
 	{
 		this->LinkedListHead = head;
 		Node *current = head;
-		int count =0;
-		while(current!=NULL)
+		int count = 0;
+		while (current != NULL)
 		{
 			count++;
 			current = current->next;
 		}
 		this->LengthOfLinkedList = count;
 	}
-	int 		Length()
+	int Length()
 	{
 		return this->LengthOfLinkedList;
 	}
-	void 		InsertNode(int data, int position)
+	void InsertNode(int data, int position)
 	{
 		Node *newNode = new Node(data);
 		if (position < 0)
@@ -201,7 +213,7 @@ public:
 		}
 		LengthOfLinkedList++;
 	}
-	void 		PrintList()
+	void PrintList()
 	{
 		Node *temp = LinkedListHead;
 		if (LinkedListHead == NULL)
@@ -216,46 +228,48 @@ public:
 		}
 		cout << "\n";
 	}
-	void 		DeleteNode(int position) 
-{ 
-   if (this->LinkedListHead == NULL) return; 
-  
-    Node* temp = this->LinkedListHead; 
-    if (position == 0) 
-    { 
-        this->LinkedListHead = temp->next;  
-        free(temp);        
-        return; 
-    } 
-    for (int i=0; temp!=NULL && i<position-1; i++) 
-         temp = temp->next; 
-    if (temp == NULL || temp->next == NULL) 
-         return; 
-    Node *next = temp->next->next; 
-    free(temp->next); 
-    temp->next = next; 
-}   
-	Node 		*ReturnLinkedListHead() 
-	{ return LinkedListHead; }
-	void 		Reverse()
+	void DeleteNode(int position)
+	{
+		if (this->LinkedListHead == NULL)
+			return;
+
+		Node *temp = this->LinkedListHead;
+		if (position == 0)
+		{
+			this->LinkedListHead = temp->next;
+			free(temp);
+			return;
+		}
+		for (int i = 0; temp != NULL && i < position - 1; i++)
+			temp = temp->next;
+		if (temp == NULL || temp->next == NULL)
+			return;
+		Node *next = temp->next->next;
+		free(temp->next);
+		temp->next = next;
+	}
+	Node *ReturnLinkedListHead()
+	{
+		return LinkedListHead;
+	}
+	void Reverse()
 	{
 		this->LinkedListHead = ReverseIterative(this->LinkedListHead);
-		//this->LinkedListHead = ReverseRecursion(this->LinkedListHead);
-
+		// this->LinkedListHead = ReverseRecursion(this->LinkedListHead);
 	}
-	void 		Sort()
+	void Sort()
 	{
-	mergeSort(&this->LinkedListHead);
+		mergeSort(&this->LinkedListHead);
 	}
-	bool 		IsLoopExist()
+	bool IsLoopExist()
 	{
 		return detectLoop(this->LinkedListHead);
 	}
-	bool 		IsPalindrome()
+	bool IsPalindrome()
 	{
 		return isPalindrome(this->LinkedListHead);
 	}
-	Node        *ReturnMiddleElement()
+	Node *ReturnMiddleElement()
 	{
 		return ReturnMiddle(this->LinkedListHead);
 	}
