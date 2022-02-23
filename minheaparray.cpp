41391cb5d32ed992de269212d791921bd8d905d1
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-02-23 14:12:59 +0600

    updated

--- minheaparray.cpp
+++ minheaparray.cpp
@@ -5,8 +5,8 @@
 
 //building a min-heap
 typedef struct heap{
-	int data[HEAP_SIZE + 1];  //the body of the heap
-	int total_elements; //the current number of elements in the heap
+	int data[HEAP_SIZE + 1];
+	int total_elements;
 }heap;
 
 void heap_insert(heap *heap, int x);
@@ -31,10 +31,8 @@ void heap_init(heap *heap){
 	}
 }
 void make_heap(heap *heap, int s[], int n){
-	int i; //counter
-	//initialize the heap
+	int i; 
 	heap_init(heap);
-	//insert the elements into the heap
 	for(i = 0; i < n; i++){
 		heap_insert(heap,s[i]);
 	}
@@ -53,13 +51,10 @@ void print_heap(heap *heap){
 int get_minimum(heap *heap){
 	return heap -> data[1];
 }
-
-//returns the index of the parent (if any) of the index n
 int get_parent(int n){
 	return n == 1 ? -1 : (int)(n/2);
 
 }
-//returns the index of the younger child
 int young_child(int n){
 	return 2*n;
 }
@@ -71,29 +66,23 @@ int remove_minimum(heap *heap){
 		printf("EMPTY HEAP!");
 
 	else{
-		//the root is the minimum element
 		min = heap -> data[1];
-		//replace the element at the top with last element in the heap
 		heap -> data[1] = heap -> data[heap -> total_elements];
-		//reduce the total elements in the heap
 		heap -> total_elements -= 1;
 		bubble_down(heap,1);
 	}
 	return min;
 }
-
-//bubbles down an element into it's proper place in the heap
 void bubble_down(heap *heap, int p){
-	int c; //child index
-	int i; //counter
-	int min_index; //index of lightest child
+	int c;
+	int i; 
+	int min_index;
 
 	c = young_child(p);
 	min_index = p;
 
 	for(i = 0; i < 2; i++){
 		if(c + i <= heap -> total_elements){
-			//check to see if the data at min_index is larger than the data at the child
 			if(heap -> data[min_index] > heap -> data[c+i]){
 				min_index = c + i;
 			}
@@ -101,32 +90,19 @@ void bubble_down(heap *heap, int p){
 	}
 
 	if(min_index != p){
-		//swap the elements
 		int temp_variable = heap -> data[p];
 		heap -> data[p] = heap -> data[min_index];
 		heap -> data[min_index] = temp_variable;
-		//call bubble down
 		bubble_down(heap, min_index);
 	}
 }
-
-//bubbles up the last element of the heap to maintain heap structure
-/**
-* *heap: pointer to the heap
-* total_elements: the total elements in the heap
-**/
 void bubble_up(heap *heap, int index){
-	//if we are at the root of the heap, no parent
 	if(get_parent(index) == -1)
 		return;
-
-	//if the parent node has a larger data value, we need to bubble up
 	if(heap -> data[get_parent(index)] > heap -> data[index]){
-		//swap the elements
 		int temp_variable = heap -> data[get_parent(index)];
 		heap -> data[get_parent(index)] = heap -> data[index];
 		heap -> data[index] = temp_variable;
-		//call the bubble up function to the new parent
 		bubble_up(heap, get_parent(index));
 	}
 }
@@ -150,11 +126,7 @@ void heap_sort(heap *heap)
 {
 	int range = heap->total_elements;
 	for(int i=0; i<range; i++)
-	{
-		int temp= get_minimum(heap);
-		remove_minimum(heap);
-		print_heap(heap);
-		heap->data[heap->total_elements]=temp;
+	{ remove_minimum(heap);
 	}
 
 }
@@ -167,10 +139,12 @@ int main(int argc, char const *argv[]){
 
 	make_heap(my_heap,my_elements,4);
 	print_heap(my_heap);
-	heap_sort(my_heap);
+	remove_minimum(my_heap);
+	print_heap(my_heap);
+	//heap_sort(my_heap);
 	// printf("%d\n",get_minimum(my_heap));
 	// remove_minimum(my_heap);
-	for(int i=0; i<4; i++) printf("%d\n",my_heap->data[i]);
+	//for(int i=0; i<4; i++) printf("%d\n",my_heap->data[i]);
 	// print_heap(my_heap);
 	// printf("%d\n",my_heap -> total_elements);
 	// free(my_heap);
